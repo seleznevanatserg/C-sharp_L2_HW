89b3547bf2d3d58bacdb125456d5137a39711885
SeleznevAS <seleznevanatserg@yahoo.com>
2022-11-25 20:36:49 +0300

    task10 edited

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5     782 -> 8    918 -> 1
+
+
+Random random = new Random();
+int number = random.Next(100, 1000);
+Console.WriteLine($"{number} -- it`s number from Random (100 - 999)");
+
+int number2 = number / 10 % 10;
+Console.WriteLine($"{number2} -- it`s 2th number from {number}");
