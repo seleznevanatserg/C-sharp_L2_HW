07e14b4aab79c3b1591cf646b0eca6974de72a64
SeleznevAS <seleznevanatserg@yahoo.com>
2022-11-25 23:12:30 +0300

    task13 edited

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5     78 -> третьей цифры нет     32679 -> 6
+
+Console.WriteLine("Enter your number, please.");
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+
+if (isParsed == true)
+{
+    if (number >= 100 | number <= -100)
+    {   
+        int number3 = number;
+        if (number3 < 0)
+        {
+            number3 = number3 * -1;
+        }
+        
+        if (number3 > 999)
+        {
+            while (number3 > 999)
+            {
+                number3 = number3 / 10;
+            }
+            number3 = number3 % 10;
+        }
+        
+        else
+        {   
+            number3 = number3 % 10;
+        }
+        
+        Console.WriteLine($"{number3} -- it`s 3th number from {number}");
+    }
+    else
+    {
+        Console.WriteLine($"Sorry, have not 3th number from {number}.");
+    }
+}
+else
+{
+    Console.WriteLine("Sorry, but You entered bullshit. Please, number enter.");
+}
+
+
